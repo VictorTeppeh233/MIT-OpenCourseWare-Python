4c3a57a1c42d2ece9c3506f83625ab7279f8c91f
VictorTeppeh233 <victorteppeh233@gmail.com>
2025-03-12 21:18:12 +0000

    add a and b then multiply by c

---
+++ Lecture-01/Assignment-01.py
@@ -0,0 +1,10 @@
+#Question
+# Assume 3 variables are already defined for you: a , b , and c .
+#  Create a variable called total that adds a and b then multiplies 
+# the result by c . Include a last line in your code to print the value: print(total)
+
+a = 10
+b = 2
+c = 5
+total = (a+b)*c 
+print(total)
