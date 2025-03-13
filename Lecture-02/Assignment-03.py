0cfa37d62f86b38eec9a0143618371217783c8c7
VictorTeppeh233 <victorteppeh233@gmail.com>
2025-03-13 22:05:59 +0000

    guess game

---
+++ Lecture-02/Assignment-03.py
@@ -0,0 +1,11 @@
+############## YOU TRY IT ###############
+# Write a program that:
+# * Saves a secret number. 
+# * Asks the user for a number guess.
+# * Prints a bool depending on whether the guess matches the secret.
+
+# your code here
+
+secret_number = 9
+guess = int(input("Take a guess: "))
+print(secret_number == guess)
