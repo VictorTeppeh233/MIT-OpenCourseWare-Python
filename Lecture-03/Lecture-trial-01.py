462479e5fee52d039f5de5a12be6c868fd11e142
VictorTeppeh233 <victorteppeh233@gmail.com>
2025-03-15 09:51:25 +0000

    forest loop game

--- Lecture-03/Lecture-trial-01.py
+++ Lecture-03/Lecture-trial-01.py
@@ -1,4 +1,6 @@
+#where is used as a variable to store the answer for direction
 where = input("You are in the Lost Forest. Go left or right? ")
+#while loop checkes for the direction and if yes repeats the code block.
 while where == "right":
     where = input("You are still in the Lost Forest. Go left or right? ")	
 print("You got out of the Lost Forest!") 
