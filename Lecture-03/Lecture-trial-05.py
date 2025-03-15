f1c1497463024b25b3ee1f44081d267df18ef140
VictorTeppeh233 <victorteppeh233@gmail.com>
2025-03-15 11:49:56 +0000

    program to display a  number input from that number to 1

---
+++ Lecture-03/Lecture-trial-05.py
@@ -0,0 +1,11 @@
+#control flow for while loops
+#iterate through numbers in a sequence
+
+#set loop variable outside while loop
+n = int(input("Enter number: "))
+#while loop
+while n > 0: #test loop variable condition. If true executes code blocks
+#executes code blocks when true
+    print(n)    #true
+#n = n+1 is equivalent to n +=1
+    n = n-1     #increment/decrement loop variable inside while loop
