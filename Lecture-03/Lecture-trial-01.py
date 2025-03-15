8dda425338cbca87ac2689b800cc059939309354
VictorTeppeh233 <victorteppeh233@gmail.com>
2025-03-15 09:46:47 +0000

    forest loop game

---
+++ Lecture-03/Lecture-trial-01.py
@@ -0,0 +1,4 @@
+where = input("You are in the Lost Forest. Go left or right? ")
+while where == "right":
+    where = input("You are still in the Lost Forest. Go left or right? ")	
+print("You got out of the Lost Forest!") 
