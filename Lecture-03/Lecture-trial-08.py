fbcc62671d715b22bc614b5bd7d501aa2e50f2e8
VictorTeppeh233 <victorteppeh233@gmail.com>
2025-03-15 17:27:11 +0000

    fixed the code to find the sums of numbers between two numbers

---
+++ Lecture-03/Lecture-trial-08.py
@@ -0,0 +1,14 @@
+################ YOU TRY IT ################
+# Fix this code to use variables start and end in the 
+# range, to get the total sum between and including those values. 
+
+mysum = 0
+i = 1
+start = int(input("start: "))
+end = int(input("end: "))
+end = end + 1
+for i in range(start, end):
+    mysum += i
+print(mysum)
+
+###########################################
