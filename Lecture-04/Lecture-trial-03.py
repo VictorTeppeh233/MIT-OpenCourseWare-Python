d4db1b7ac59e924e1a7229f8acc2608a3906cfc6
VictorTeppeh233 <victorteppeh233@gmail.com>
2025-03-17 19:12:33 +0000

    search through a string for specific letters 2

---
+++ Lecture-04/Lecture-trial-03.py
@@ -0,0 +1,6 @@
+# search through a string.
+s = "demo loops - fruit loops"
+# for loop to print when letters "i" or "u" are found
+for char in s:
+    if char == "i" or char == "u":
+        print("There is an i or u")
