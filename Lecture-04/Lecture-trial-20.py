f431527b437bda0e6e2932bbc23a9218261f29fa
Victor Teppeh <victorteppeh233@gmail.com>
2025-03-29 12:19:04 +0000

    a program to convert positive numbers to binary

--- Lecture-04/Lecture-trial-20.py
+++ Lecture-04/Lecture-trial-20.py
@@ -1,1 +1,10 @@
-print("hello world")
+###Convert to binary
+###Only Positive numbers 
+num = int(input("enter an integer: "))
+result = ''
+if num == 0:
+    result = '0'
+while num > 0:
+    result = str(num%2) + result
+    num = num//2
+print (result)
